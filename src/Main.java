da24269d0e08acf6f999691c0f069763e7cef6a5
YANDIRA SUMA <81127059+Yandira-suma@users.noreply.github.com>
2023-01-23 13:55:19 -0500

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Coche miCoche=new Coche();
+        miCoche.numeroPuertas = 5;
 
         miCoche.incrementarPuertas();
-        System.out.println(miCoche.numeroPuertas);
+        System.out.println("el numero de puertas es: " +miCoche.numeroPuertas);
 
     }
 }
 class Coche{
-    public int numeroPuertas=5;
+    public int numeroPuertas;
     public void incrementarPuertas(){
         this.numeroPuertas++;
     }
